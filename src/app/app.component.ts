ed346653c3e9c7d50ac4ba83a02bb27fd94847a8
ecarpintero1979 <ecarpintero@gmail.com>
2022-03-09 11:16:15 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 // 1.) Convertir este proyecto mono modulo -> mono componente -> con separación de responsabilidades
 
 // 2.) Convertir este proyecto -> nono modulo -> multicomponentes
@@ -11,13 +10,11 @@ import { Component } from '@angular/core';
 //          c2) TEMPERATURA
 //          c2) CLIMATOLOGIA
 
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  
+  temp = '21';
 }
